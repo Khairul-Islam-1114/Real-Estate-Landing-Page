bd9abe5c81b74ede776c48c70cfba233d311aa5f
Khairul-Islam-1114 <khairulmuhammad1114@gmail.com>
2024-04-14 22:53:19 +0600

    Add Navbar Toggler for small screen

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,31 +6,38 @@ const Header = () => {
     <nav className='navbar navbar-expand-sm bg-light'>
         <div className="container-fluid">
 
-            <a href="#" className="navbar-brand text-info"><strong>McRent</strong></a>
+            <a href="#home" className="navbar-brand text-info"><strong>McRent</strong></a>
 
-            <ul className="navbar-nav">
+            <button type='button' className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarIcon">
+                 <span className="navbar-toggler-icon"></span>
 
-                <li className="nav-item">
-                    <a href="#home" className="nav-link active text-info" style={{fontWeight:500}}>Home</a>
-                </li>
+            </button>
 
-                <li className="nav-item">
-                    <a href="#about_us" className="nav-link">About Us</a>
-                </li>
+            <div className="collapse navbar-collapse" id="navbarIcon">
+                <ul className="navbar-nav ms-auto">
 
-                <li className="nav-item">
-                    <a href="#services" className="nav-link">Our Service</a>
-                </li>
+                    <li className="nav-item">
+                        <a href="#home" className="nav-link active text-info" style={{fontWeight:500}}>Home</a>
+                    </li>
 
-                <li className="nav-item">
-                    <a href="#contact" className="nav-link">Contact</a>
-                </li>
-            </ul>
+                    <li className="nav-item">
+                        <a href="#about_us" className="nav-link">About Us</a>
+                    </li>
 
-            <div className="user-info">
-                <a className="btn btn-light" href='#'>Sign In</a>
-                <a className="btn btn-info text-white" href='#'>Sign Up</a>
+                    <li className="nav-item">
+                        <a href="#services" className="nav-link">Our Service</a>
+                    </li>
 
+                    <li className="nav-item">
+                        <a href="#contact" className="nav-link">Contact</a>
+                    </li>
+                </ul>
+
+                <div className="user-info ms-auto">
+                    <a className="btn btn-light" href='#'>Sign In</a>
+                    <a className="btn btn-info text-white" href='#'>Sign Up</a>
+
+                </div>
             </div>
         </div>
     </nav>
